03deea07570e277d815ec2e10c7db0fee36cb43f
phamdinhduyen <pdinhduyen@gmail.com>
2023-04-14 20:48:03 +0700

    update seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 use Illuminate\Support\Facades\DB;
+use Faker\Factory;
+
 
 class UserSeeder extends Seeder
 {
@@ -19,25 +21,27 @@ class UserSeeder extends Seeder
     
     public function run()
     {
-        function RandomName() {
-            $keys = array_merge(range('a', 'z'));
-            shuffle($keys);
-            $randomString = implode('', array_slice($keys, 0, 10));
-            while(strlen($randomString) < 10) {
-                shuffle($keys);
-                $randomString .= implode('', array_slice($keys, 0, 10 - strlen($randomString)));
-            }
-            return $randomString;
-        }
+        // function RandomName() {
+        //     $keys = array_merge(range('a', 'z'));
+        //     shuffle($keys);
+        //     $randomString = implode('', array_slice($keys, 0, 10));
+        //     while(strlen($randomString) < 10) {
+        //         shuffle($keys);
+        //         $randomString .= implode('', array_slice($keys, 0, 10 - strlen($randomString)));
+        //     }
+        //     return $randomString;
+        // }
+        $faker = Factory::create();
 
 
        for($i = 1; $i <= 10; $i++){
         DB::table('users')->insert([
-            'fullname' => RandomName(),
-            'email' => RandomName() . '@gmail.com',
+            'fullname' =>  $faker->name,
+            'email' =>  $faker->email,
             'group_id' => 1,
             'status' => 0,
-            'country_id' => 1
+            'country_id' => 1,
+            'created_at'=> now()
         ]);
     }
     }
